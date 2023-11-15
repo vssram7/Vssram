2daecf0a83001f88adf47854bc6aff0897167db6
vssram7 <131565553+vssram7@users.noreply.github.com>
2023-11-15 11:26:36 +0530

    Delete force-app/main/default/lwc/testContainer directory

--- force-app/main/default/lwc/testContainer/testContainer.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement } from "lwc";
-
-export default class PreviewModal extends LightningElement {
-  handleShowModal() {
-    const modal = this.template.querySelector("c-modalPopup");
-    modal.show();
-  }
-}
